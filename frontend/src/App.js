eb1cd76975b00800442a754bb31725fcc64212b3
a1881053 <ngocthuytram.nguyen@student.adelaide.edu.au>
2025-09-04 09:12:51 +0930

    Modify CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,24 @@
 import './App.css';
+import Dashboard from './components/Dashboard';
 import FeaturesGrid from './components/FeatureGrid';
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 import WelcomeSection from './components/WelcomeSection';
 
 function App() {
-  // const [message, setMessage] = useState('');
-
-  // useEffect(() => {
-  //   fetch("/api/hello")
-  //     .then(res => res.text())
-  //     .then(data => setMessage(data))
-  //     .catch(err => console.error(err));
-  // }, []);
-
   return (
-    <div>
+    <div className="app-container">
       <NavBar />
-      <WelcomeSection />
-      <FeaturesGrid />
-      <Footer />
+      <main className="app-main">
+        <WelcomeSection />
+        <FeaturesGrid />
+        <Dashboard />
+      </main>
+      {/* <Footer /> */}
     </div>
 
   );
 }
+
 
 export default App;
